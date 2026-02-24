3c9a942fa96edf0a63c5b3d65e51edbe612bc704
vijju <vijjududala@gmail.com>
2026-02-24 19:46:08 +0530

    auto create admin user

--- config/settings.py
+++ config/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'movies',
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
---
+++ movies/create_superuser.py
@@ -0,0 +1,10 @@
+from django.contrib.auth.models import User
+
+def run():
+    if not User.objects.filter(username="admin").exists():
+        User.objects.create_superuser(
+            username="admin",
+            email="admin@gmail.com",
+            password="admin123"
+        )
+        print("Superuser created!")
